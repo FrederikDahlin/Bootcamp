07942eab289d07a90b884e9e9c5dd1543a10097d
FrederikDahlin <4027010@RTS-365.dk>
2018-01-19 09:12:54 +0100

    Opgave 7

--- variabler.js
+++ variabler.js
@@ -1,7 +1,7 @@
-// let navn = "Frederik";
+let navn = "Frederik";
 // console.log(navn);
 
-// let alder = 20;
+let alder = 20;
 // console.log(alder);
 
 // let byNavn = "Roskilde";
@@ -11,7 +11,12 @@
 // let y = 10;
 // console.log(x + y)
 
-let x = 5;
-let y = 10;
-let z = x + y;
-console.log(z)
+// let x = 5;
+// let y = 10;
+// let z = x + y;
+// console.log(z)
+
+let mitFornavn = navn;
+let mitEfternavn = "Dahlin";
+let minAlder = alder
+console.log('Hej, jeg hedder ' + mitFornavn + ' ' + mitEfternavn+ ' og jeg er ' + minAlder + ' år');
