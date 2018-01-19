2d2cee74d4f6bb6d3d27fbf9f269b009bcdaa3ec
FrederikDahlin <4027010@RTS-365.dk>
2018-01-19 11:14:00 +0100

    Opgave 7

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-let frugter = ["Pære", "Æble", "Banan"];
+// let frugter = ["Pære", "Æble", "Banan"];
 // console.log(frugter);
 
 // console.log(frugter[1]);
@@ -8,5 +8,8 @@ let frugter = ["Pære", "Æble", "Banan"];
 
 // console.log(frugter.length);
 
-frugter[3] = "Appelsin";
-console.log(frugter);
+// frugter[3] = "Appelsin";
+// console.log(frugter);
+
+let personer = [["Mark", 20, "KBH"],["Lars", 30, "Nyhavn"],["Lukas", 33, "Herlev"]];
+console.log(personer);
