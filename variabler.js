cbdd5da70e276ac1329ce41dc5c1aea4904442c8
FrederikDahlin <4027010@RTS-365.dk>
2018-01-19 09:03:00 +0100

    Opgave 4

--- variabler.js
+++ variabler.js
@@ -1,5 +1,8 @@
 // let navn = "Frederik";
 // console.log(navn);
 
-let alder = 20;
-console.log(alder);
+// let alder = 20;
+// console.log(alder);
+
+let byNavn = "Roskilde";
+console.log(byNavn);
