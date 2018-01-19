c6eac908120ad0e4c7dfaddd20f2b6ea4264123d
FrederikDahlin <4027010@RTS-365.dk>
2018-01-19 12:49:13 +0100

    Opgave 4

--- betingelser.js
+++ betingelser.js
@@ -7,12 +7,17 @@
 //     console.log("Nej");
 // }
 
-let tal = 3;
-console.log(tal)
+// let tal = 3;
+// console.log(tal)
 
-if(tal > 5) {
-    console.log("Højere end 5");
-}
-else {
-    console.log("Lavere end 5");
-}
+// if(tal > 5) {
+//     console.log("Højere end 5");
+// }
+// else {
+//     console.log("Lavere end 5");
+// }
+
+let boolean = true;
+
+let result = boolean ? "True" : "False";
+console.log(result);
