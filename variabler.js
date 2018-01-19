3de341da297df9bd832b7d3439820515580ea666
FrederikDahlin <4027010@RTS-365.dk>
2018-01-19 09:41:47 +0100

    Opgave 9

--- variabler.js
+++ variabler.js
@@ -1,29 +1,35 @@
-// let navn = "Frederik";
-// // console.log(navn);
+// // let navn = "Frederik";
+// // // console.log(navn);
 
-// let alder = 20;
-// // console.log(alder);
+// // let alder = 20;
+// // // console.log(alder);
 
-// // let byNavn = "Roskilde";
-// // console.log(byNavn);
+// // // let byNavn = "Roskilde";
+// // // console.log(byNavn);
 
-// // let x = 5;
-// // let y = 10;
-// // console.log(x + y)
+// // // let x = 5;
+// // // let y = 10;
+// // // console.log(x + y)
 
-// // let x = 5;
-// // let y = 10;
-// // let z = x + y;
-// // console.log(z)
+// // // let x = 5;
+// // // let y = 10;
+// // // let z = x + y;
+// // // console.log(z)
 
-// let mitFornavn = navn;
-// let mitEfternavn = "Dahlin";
-// let minAlder = alder
-// console.log('Hej, jeg hedder ' + mitFornavn + ' ' + mitEfternavn+ ' og jeg er ' + minAlder + ' år');
+// // let mitFornavn = navn;
+// // let mitEfternavn = "Dahlin";
+// // let minAlder = alder
+// // console.log('Hej, jeg hedder ' + mitFornavn + ' ' + mitEfternavn+ ' og jeg er ' + minAlder + ' år');
 
-let moms = 1.25;
-let prisUdenMoms = 400;
-let prisMedMoms = prisUdenMoms * moms;
-console.log(prisMedMoms);
-let prisMedMoms2 = prisUdenMoms / moms;
-console.log(prisMedMoms2);
+// let moms = 1.25;
+// let prisUdenMoms = 400;
+// let prisMedMoms = prisUdenMoms * moms;
+// console.log(prisMedMoms);
+// let prisMedMoms2 = prisUdenMoms / moms;
+// console.log(prisMedMoms2);
+
+let forste = 10;
+let anden = 20;
+anden = forste;
+forste = forste+forste;
+console.log(forste, anden);
