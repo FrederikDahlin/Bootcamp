bd94db4bb25db375a859414847688f4d2a97ee09
FrederikDahlin <4027010@RTS-365.dk>
2018-01-23 10:05:00 +0100

    Opgave 4

--- operatorer.js
+++ operatorer.js
@@ -2,7 +2,24 @@
 // let anden = 7;
 // console.log(forste + anden);
 
-let forste = 39;
-let anden = 11;
-forste %= anden;
-console.log(forste);
+// let forste = 39;
+// let anden = 11;
+// forste %= anden;
+// console.log(forste);
+
+let navn = "Mads";
+let efternavn = "Larsen";
+if(navn ==   "Mads" && efternavn == "Larsen"){
+    console.log("if");
+}
+else {
+    console.log("else");
+}
+let x = 20;
+let y = 7;
+if(x > y && 5 < y){
+    console.log("if");
+}
+else {
+    console.log("else");
+}
